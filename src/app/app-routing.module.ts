6b84bd5078b9b684c10f082ed88a6cff90485380
Dee Mullins <mullins.dee@gmail.com>
2018-09-19 09:00:05 -0700

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+const routes: Routes = [
+  {
+    path: 'task',
+    component: TaskFormComponent
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: [],
   exports: [RouterModule]
